413ccf6e13ff78fd6e76348670f4b7c679489967
Hugo Freire <hfreire@exec.sh>
2019-06-22 10:44:16 +0200

    chore(terraform): rename outputs file

--- share/terraform/outputs.tf
+++ share/terraform/outputs.tf
@@ -1,23 +1,23 @@
 output "aws_region" {
-  value = "${var.aws_region}"
+  value = var.aws_region
 
   sensitive = true
 }
 
 output "aws_vpc_id" {
-  value = "${data.aws_vpc.selected.id}"
+  value = data.aws_vpc.selected.id
 
   sensitive = true
 }
 
 output "aws_subnet_ids" {
-  value = "${data.aws_subnet_ids.selected.ids}"
+  value = data.aws_subnet_ids.selected.ids
 
   sensitive = true
 }
 
 output "aws_security_group_ids" {
-  value = "${data.aws_security_groups.selected.ids}"
+  value = data.aws_security_groups.selected.ids
 
   sensitive = true
 }
