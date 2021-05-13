f8596562a6a7de75e406bcc019b6ae6da8480c47
Hugo Freire <hfreire@exec.sh>
2021-05-13 10:32:44 +0200

    fix(github): minor tweaks

--- test/handler.js
+++ test/handler.js
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2019, Hugo Freire <hugo@exec.sh>.
+ * Copyright (c) 2021, Hugo Freire <hugo@exec.sh>.
  *
  * This source code is licensed under the license found in the
  * LICENSE.md file in the root directory of this source tree.
@@ -19,8 +19,8 @@ describe('Handler', () => {
     const token = 'my-token'
 
     beforeAll(() => {
-      process.env.GH_ORGS = orgs
-      process.env.GH_TOKEN = token
+      process.env.GITHUB_HANDYMAN_WHITELISTED_ORGS = orgs
+      process.env.GITHUB_HANDYMAN_GITHUB_TOKEN = token
     })
 
     beforeEach(() => {
