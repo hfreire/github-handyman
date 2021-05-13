5f44d14a4ca0873d68dcdacabd74df9e261295a6
Hugo Freire <hfreire@exec.sh>
2021-05-13 10:26:34 +0200

    fix(github): minor tweaks

--- src/handler.js
+++ src/handler.js
@@ -1,21 +1,21 @@
 /*
- * Copyright (c) 2019, Hugo Freire <hugo@exec.sh>.
+ * Copyright (c) 2021, Hugo Freire <hugo@exec.sh>.
  *
  * This source code is licensed under the license found in the
  * LICENSE.md file in the root directory of this source tree.
  */
 
-const GH_ORGS = process.env.GH_ORGS
-const GH_TOKEN = process.env.GH_TOKEN
+const WHITELISTED_ORGS = process.env.GITHUB_HANDYMAN_WHITELISTED_ORGS
+const GITHUB_TOKEN = process.env.GITHUB_HANDYMAN_GITHUB_TOKEN
 
 const _ = require('lodash')
 
 const GitHubHandyman = require('./github-handyman')
 
-const orgs = _.words(GH_ORGS, /[^, ]+/g) || []
+const orgs = _.words(WHITELISTED_ORGS, /[^, ]+/g) || []
 const owners = [ undefined ].concat(orgs)
 
-const handyman = new GitHubHandyman({ github: { octokit: { auth: GH_TOKEN } } })
+const handyman = new GitHubHandyman({ github: { octokit: { auth: GITHUB_TOKEN } } })
 handyman.on('error', (error, owner, repo, { url }) => console.warn(`Failed to help out with pull request ${url} because of ${error.message}`))
 handyman.on('pulls:create', (owner, repo, title) => console.info(`Created pull request ${owner}/${repo} ${title}`))
 handyman.on('pulls:merge', (owner, repo, number) => console.info(`Merged pull request ${owner}/${repo} ${number}`))
